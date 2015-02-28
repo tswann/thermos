3ff9bdd763a009756572e1d0ecf604a79c59cc53
Thomas Swann <ts.swann@gmail.com>
2015-02-28 21:51:50 +0000

    Added macro for field rendering

--- thermos/forms.py
+++ thermos/forms.py
@@ -4,5 +4,20 @@ from flask.ext.wtf.html5 import URLField
 from wtforms.validators import DataRequired, url
 
 class BookmarkForm(Form):
-    url = URLField('url', validators=[DataRequired(), url()])
-    description = StringField('description')
+    url = URLField('The URL for your bookmark:', validators=[DataRequired(), url()])
+    description = StringField('Add an optional description:')
+
+    # Override validate and provide sensible default behaviour
+    # for incomplete url and description
+    def validate(self):
+        if not self.url.data.startsWith('http://') or\
+            self.url.data.startsWith('https://'):
+            self.url.data = 'http://' + self.url.data
+
+        if not Form.validate(self):
+            return False
+
+        if not self.description.data:
+            self.description.data = self.url.data
+
+        return True
