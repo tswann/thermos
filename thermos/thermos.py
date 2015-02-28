4fedd39ed9c3cacd8f8a555fd5c647091885979c
Thomas Swann <ts.swann@gmail.com>
2015-02-28 22:17:39 +0000

    Added SQLAlchemy model classes

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -1,9 +1,15 @@
+import os
 from flask import Flask, render_template, url_for, request, redirect, flash
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 from forms import BookmarkForm
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '\xaa\x1d\xa8\xe5\xe5\xac!m\x1b\xf30P\x03Z\x8d.\x94\xa7P\x07\xc8G\x152'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'thermos.db')
+db = SQLAlchemy(app)
 
 bookmarks = []
 
