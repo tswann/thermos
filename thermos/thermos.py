1b8d9d17178e18e7c71b94e7b113b97fe6a6976c
Thomas Swann <ts.swann@gmail.com>
2015-02-28 16:32:48 +0000

    Added WTForms implementation

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -1,41 +1,38 @@
 from flask import Flask, render_template, url_for, request, redirect, flash
 from datetime import datetime
+from forms import BookmarkForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '\xaa\x1d\xa8\xe5\xe5\xac!m\x1b\xf30P\x03Z\x8d.\x94\xa7P\x07\xc8G\x152'
-
-class User:
-    def __init__(self, firstname, lastname):
-        self.firstname = firstname
-        self.lastname = lastname
-
-    def initials(self):
-        return "{}, {}.".format(self.firstname[0], self.lastname[0])
 
 bookmarks = []
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html',
-                           title="Title passed from view to template",
-                           user=User("Tom", "Swann"))
+    return render_template('index.html', new_bookmarks=new_bookmarks(5))
 
-def store_bookmark(url):
+def new_bookmarks(num):
+    return sorted(bookmarks, key=lambda bm: bm['date'], reverse=True)[:num]
+
+def store_bookmark(url, description):
     bookmarks.append(dict(
                      url = url,
                      user = "tom",
+                     description = description,
                      date = datetime.utcnow()
                      ))
 
 @app.route('/add', methods = ['GET', 'POST'])
 def add():
-    if request.method == "POST":
-        url = request.form['url']
-        store_bookmark(url)
-        flash("Stored bookmark: '{}'".format(url))
+    form = BookmarkForm()
+    if form.validate_on_submit():
+        url = form.url.data
+        description = form.description.data
+        store_bookmark(url, description)
+        flash("Stored '{}'".format(description))
         return redirect(url_for('index'))
-    return render_template('add.html')
+    return render_template('add.html', form=form)
 
 @app.errorhandler(404)
 def page_not_found(e):
