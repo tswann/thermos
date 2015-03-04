0b2644977b5ab1678a1362e64587ddb3a7c276f5
Thomas Swann <ts.swann@gmail.com>
2015-03-04 11:13:11 +0000

    Added signup page

--- thermos/forms.py
+++ thermos/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import Form
 from wtforms.fields import StringField, BooleanField, SubmitField, PasswordField
-from wtforms.validators import URL, DataRequired
+from wtforms.validators import URL, DataRequired, Length, Regexp, EqualTo, Email, ValidationError
+from models import User
 
 class BookmarkForm(Form):
     url = StringField('The URL for your bookmark:', validators=[URL(message='Sorry, Invalid URL.')])
@@ -26,3 +27,26 @@ class LoginForm(Form):
     password = PasswordField('Password:', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log In')
+
+class SignupForm(Form):
+    username = StringField ('Username',
+                            validators=[
+                                DataRequired(), Length(3, 80),
+                                Regexp('^[A-Za-z0-9_]{3,}$',
+                                       message='Usernames consist of numbers, letters and underscores.')
+                            ])
+    password = PasswordField('Password',
+                             validators=[
+                                 DataRequired(),
+                                 EqualTo('password2', message='Passwords must match.')
+                             ])
+    password2 = PasswordField('Confirm Password', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Length(1,120), Email()])
+
+    def validate_email(self, email_field):
+        if User.query.filter_by(email=email_field.data).first():
+            raise ValidationError('There is already a user with this email address.')
+
+    def validate_username(self, username_field):
+        if User.get_by_username(username_field.data):
+            raise ValidationError('This username is already taken.')
--- thermos/views.py
+++ thermos/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, request, redirect, flash
 from thermos import app, db, login_manager
-from forms import BookmarkForm, LoginForm
+from forms import BookmarkForm, LoginForm, SignupForm
 from models import User, Bookmark
 from flask_login import login_required, login_user, logout_user, current_user
 
@@ -44,6 +44,19 @@ def login():
             return redirect(request.args.get('next') or url_for('user', username=user.username))
         flash('Incorrect username or password.')
     return render_template("login.html", form=form)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Welcome, {}! Please login.'.format(user.username))
+        return redirect(url_for('login'))
+    return render_template("signup.html", form=form)
 
 @app.route('/logout')
 def logout():
